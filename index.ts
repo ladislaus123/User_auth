9225b97fbea8d31aa48a81ca24c00aa657d35798
ladislaus123 <chrisladlu1001@gmail.com>
2025-08-26 10:15:22 -0300

    connected to mongodb

--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
 import express, { Request, Response } from 'express';
+import "dotenv/config"
+import mongoose from 'mongoose';
+
+const url = process.env.DATABASE_URL as string;
+
+const connect_to_db = async() =>{
+   try {
+     await mongoose.connect(url)
+     console.log("Connected to mongo db")
+
+   } catch (error) {
+    console.error("database onnection failed: " + error)
+    process.exit(1)
+   }
+};
+
+connect_to_db();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
