939035d130c7d4bc5a82a0710f5e705355bf8700
ladislaus123 <chrisladlu1001@gmail.com>
2025-08-26 10:37:10 -0300

    added a get username and password

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express';
 import "dotenv/config"
 import mongoose from 'mongoose';
+import router from './routes/validation';
+
+const app = express();
 
 const url = process.env.DATABASE_URL as string;
+
+app.use(express.json())
 
 const connect_to_db = async() =>{
    try {
@@ -10,21 +15,19 @@ const connect_to_db = async() =>{
      console.log("Connected to mongo db")
 
    } catch (error) {
-    console.error("database onnection failed: " + error)
+    console.error("database onnection failed: ", error)
     process.exit(1)
    }
 };
 
 connect_to_db();
 
-const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Express with TypeScript!');
-});
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
